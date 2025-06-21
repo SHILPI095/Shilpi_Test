bad98f8453c2aabe8a748a6a43f9412093f27eb3
SHILPI095 <shilpitiwari04@gmail.com>
2025-06-21 12:53:53 +0530

    Update Insert_table.sql

--- Insert_table.sql
+++ Insert_table.sql
@@ -2,4 +2,14 @@ INSERT INTO Persons (PersonID, LastName, FirstName, Address, City)
 VALUES (1, 'Wilson', 'John', '123 Main St.', 'Anytown'),
        (2, 'Smith', 'Mary', '456 Maple St.', 'Anytown'),
        (3, 'Jones', 'David', '789 Elm St.', 'Anytown'),
-       (4, 'John', 'David', '789 Elm St.', 'Meru');
+       (4, 'John', 'David', '789 Elm St.', 'Meru'),
+      (5, 'Wilson', 'John', '123 Main St.', 'Anytown'),
+       (6, 'Smith', 'Mary', '456 Maple St.', 'Anytown'),
+       (7, 'Jones', 'David', '789 Elm St.', 'Anytown'),
+       (8, 'John', 'David', '789 Elm St.', 'Anytown'),
+       (9, 'Wilson', 'John', '123 Main St.', 'Anytown'),
+       (10, 'Smith', 'Mary', '456 Maple St.', 'Anytown'),
+       (11, 'Jones', 'David', '789 Elm St.', 'Anytown'),
+       (12, 'John', 'David', '789 Elm St.', 'Anytown'),
+       (13, 'Wilson', 'John', '123 Main St.', 'Anytown'),
+       (14, 'Smith', 'Mary', '456 Maple St.', 'Anytown');

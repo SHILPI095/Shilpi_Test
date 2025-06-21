a99a68a222e76f259a121ceeef8a535cb9f4f273
SHILPI095 <shilpitiwari04@gmail.com>
2025-06-21 12:53:03 +0530

    Create Insert_table.sql

---
+++ Insert_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO Persons (PersonID, LastName, FirstName, Address, City)
+VALUES (1, 'Wilson', 'John', '123 Main St.', 'Anytown'),
+       (2, 'Smith', 'Mary', '456 Maple St.', 'Anytown'),
+       (3, 'Jones', 'David', '789 Elm St.', 'Anytown'),
+       (4, 'John', 'David', '789 Elm St.', 'Meru');

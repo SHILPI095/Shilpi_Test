624df45e704e336d941f8be9aa1b1332f458a98d
SHILPI095 <shilpitiwari04@gmail.com>
2025-06-21 12:52:24 +0530

    Create Create_table.sql

---
+++ Create_table.sql
@@ -0,0 +1,9 @@
+------------This contains codes for sql
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
